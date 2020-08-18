536303133d1d31e52f1142058049683bd3f92224
Varshi Bachu <vabachu@microsoft.com>
2020-08-18 14:48:23 -0700

    made changes to JS and TS entity files and other minor fixes

--- Functions.Templates/Templates/DurableFunctionsEntityOrchestration-CSharp-2.x/DurableFunctionsEntityOrchestrationCSharp.cs
+++ Functions.Templates/Templates/DurableFunctionsEntityOrchestration-CSharp-2.x/DurableFunctionsEntityOrchestrationCSharp.cs
@@ -1,50 +1,57 @@
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 
-namespace DurableEntitiesTemplate
+namespace Company.Function
 {
-    [FunctionName("CounterOrchestration")]
-    public static async Task Run(
-    [OrchestrationTrigger] IDurableOrchestrationContext context)
+    public static class DurableFunctionsEntityOrchestrationCSharp
     {
-        var entityId = new EntityId(nameof(Counter), "myCounter");
-
-        // Two-way call to the entity which returns a value - awaits the response
-        int currentValue = await context.CallEntityAsync<int>(entityId, "Get");
-        if (currentValue < 10)
+        [FunctionName("DurableFunctionsEntityOrchestrationCSharp_CounterOrchestrator")]
+        public static async Task Orchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            // One-way signal to the entity which updates the value - does not await a response
-            context.SignalEntity(entityId, "Add", 1);
+            var entityId = new EntityId(nameof(Counter), "myCounter");
+
+            // Two-way call to the entity which returns a value - awaits the response
+            int currentValue = await context.CallEntityAsync<int>(entityId, "get");
+            if (currentValue < 10)
+            {
+                // One-way signal to the entity which updates the value - does not await a response
+                context.SignalEntity(entityId, "add", 1);
+            }
         }
-    }
 
-    [FunctionName("CounterHttpStart")]
-    public static async Task<HttpResponseMessage> Run(
-    [HttpTrigger(AuthorizationLevel.Function)] HttpRequestMessage req,
-    [DurableClient] IDurableOrchestrationClient client)
-    {
-        string instanceId = await client.StartNewAsync("CounterOrchestration", null);
-
-        log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
-
-        return client.CreateCheckStatusResponse(req, instanceId);
-    }
-
-    [FunctionName("Counter")]
-    public static void Counter([EntityTrigger] IDurableEntityContext ctx)
-    {
-        switch (ctx.OperationName.ToLowerInvariant())
+        [FunctionName("DurableFunctionsEntityOrchestrationCSharp_CounterHttpStart")]
+        public static async Task<HttpResponseMessage> Run(
+        [HttpTrigger(AuthorizationLevel.Function)] HttpRequestMessage req,
+        [DurableClient] IDurableOrchestrationClient client,
+        ILogger log)
         {
-            case "add":
-                ctx.SetState(ctx.GetState<int>() + ctx.GetInput<int>());
-                break;
-            case "reset":
-                ctx.SetState(0);
-                break;
-            case "get":
-                ctx.Return(ctx.GetState<int>());
-                break;
+            string instanceId = await client.StartNewAsync("DurableFunctionsEntityOrchestrationCSharp_CounterOrchestrator", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return client.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        [FunctionName("Counter")]
+        public static void Counter([EntityTrigger] IDurableEntityContext ctx)
+        {
+            switch (ctx.OperationName.ToLowerInvariant())
+            {
+                case "add":
+                    ctx.SetState(ctx.GetState<int>() + ctx.GetInput<int>());
+                    break;
+                case "reset":
+                    ctx.SetState(0);
+                    break;
+                case "get":
+                    ctx.Return(ctx.GetState<int>());
+                    break;
+            }
         }
     }
 }
